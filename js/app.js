71da0a11b0853a76dc635c29460f9c2a0bf24107
Danica Stanković <danica.stankovic.94@gmail.com>
2021-12-26 23:32:05 +0100

    .

--- js/app.js
+++ js/app.js
@@ -20,17 +20,8 @@ document.getElementById('aforizam_gosta2').innerText = "Од вековних о
 document.getElementById('aforizam_gosta3').innerText = "Пун новчаник се лакше носи.";
 
 document.getElementById('ime_gosta').innerText = "БРАНКА ТАНАСИЋ";
-document.getElementById('ime_gosta').href = "https://www.rastko.rs/rastko/delo/16033";
+document.getElementById('ime_gosta').href = "";
 document.getElementById('ime_gosta').target = "_blank";
 document.getElementById('pesma_gosta').innerHTML = "Танана су сва," + "<br>" + "плетива моја," + "<br>" + "тананијем мислим" + "<br>" + "исплетена," + "<br>" + "звезданијем небом" + "<br>" + "окићена," + "<br>" + "месечевим светлом" + "<br>" + "обасјана," + "<br>" + "у кораку вечном" + "<br>" + "битисала." + "<br>" + "Умни сусрет" + "<br>" + "две душе је празник," + "<br>" + "знак је неба," + "<br>" + "његове доброте." + "<br>" + "Уживајмо у чину" + "<br>" + "лепоте.";
-
-/*
-document.getElementById('ime_gosta4').innerText = "ДОЈЧИНОВИЋ ДУШАН";
-document.getElementById('ime_gosta4').href = "https://medijacentar016.com/index.php/2019/05/03/dusan-dojcinovic-pisac-i-aforisticar-tanja-probudi-se-prica-objavljena-u-zborniku-alma/";
-document.getElementById('ime_gosta4').target = "_blank";
-document.getElementById('aforizam_gosta10').innerText = "Пишите слободно, само пазите да Вас не провале!";
-document.getElementById('aforizam_gosta11').innerText = "Није срамота кад те ухвате да преписујеш, срамота је кад добијеш петицу, а не знају да си преписао.";
-document.getElementById('aforizam_gosta12').innerText = "Навикао сам ја на старе ципеле. Бушне су и пропуштају кишу.";
-*/
 
 console.log(val);
