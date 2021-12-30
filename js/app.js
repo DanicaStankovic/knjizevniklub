700b206715b8cf177b96274d6da54af3a4e94d2f
Danica Stanković <danica.stankovic.94@gmail.com>
2021-12-30 07:06:04 +0100

    .

--- js/app.js
+++ js/app.js
@@ -4,24 +4,43 @@ val = document;
 
 document.getElementById('aforisticar').innerText = "САВО МАРТИНОВИЋ";
 document.getElementById('aforisticar').href = "";
-document.getElementById('aforisticar').target = "_blank";
+//document.getElementById('aforisticar').target = "_blank";
 document.getElementById('af_meseca').innerText = "Што ташта роди, то мајка не рађа.";
 
 document.getElementById('pesnik').innerText = "ЉИЉАНА БОШКОВИЋ";
 document.getElementById('pesnik').href = "";
-document.getElementById('pesnik').target = "_blank";
+//document.getElementById('pesnik').target = "_blank";
 document.getElementById('st_meseca').innerHTML = "Имала Нана широко срце" + "<br>" + "Да нас све њиме много воли," + "<br>" + "И малу собу, икона пуну" + "<br>" + "Да се пред њима за нас моли.";
 
 document.getElementById('ime_gosta1').innerText = "МИТАР ЂЕРИЋ";
 document.getElementById('ime_gosta1').href = "";
-document.getElementById('ime_gosta1').target = "_blank";
+//document.getElementById('ime_gosta1').target = "_blank";
 document.getElementById('aforizam_gosta1').innerText = "Влада је увела ред у породилиштима. Уопште нема гужве.";
 document.getElementById('aforizam_gosta2').innerText = "Од вековних огњишта, остао нам је само вечни пламен.";
 document.getElementById('aforizam_gosta3').innerText = "Пун новчаник се лакше носи.";
 
-document.getElementById('ime_gosta').innerText = "БРАНКА ТАНАСИЋ";
-document.getElementById('ime_gosta').href = "";
-document.getElementById('ime_gosta').target = "_blank";
-document.getElementById('pesma_gosta').innerHTML = "Танана су сва," + "<br>" + "плетива моја," + "<br>" + "тананијем мислим" + "<br>" + "исплетена," + "<br>" + "звезданијем небом" + "<br>" + "окићена," + "<br>" + "месечевим светлом" + "<br>" + "обасјана," + "<br>" + "у кораку вечном" + "<br>" + "битисала." + "<br>" + "Умни сусрет" + "<br>" + "две душе је празник," + "<br>" + "знак је неба," + "<br>" + "његове доброте." + "<br>" + "Уживајмо у чину" + "<br>" + "лепоте.";
+document.getElementById('ime_gosta2').innerText = "НАДА КАРАЏИЋ";
+document.getElementById('ime_gosta2').href = "";
+//document.getElementById('ime_gosta2').target = "_blank";
+document.getElementById('aforizam_gosta4').innerText = "Историја се понавља, зато што јој није суђено на време.";
+document.getElementById('aforizam_gosta5').innerText = "Коме закон лежи у топузу, тај једино чува своју гузу.";
+document.getElementById('aforizam_gosta6').innerText = "Судови су нам прљави. Нема ко да их опере.";
+
+document.getElementById('ime_pesnika').innerHTML = "БРАНКА ТАНАСИЋ"+ "<br><br>" + "Плетиво неба";
+document.getElementById('ime_pesnika').href = "";
+//document.getElementById('ime_pesnika').target = "_blank";
+document.getElementById('pesma').innerHTML = "Танана су сва," + "<br>" + "плетива моја," + "<br>" + "тананијем мислим" + "<br>" + "исплетена," + "<br>" + "звезданијем небом" + "<br>" + "окићена," + "<br>" + "месечевим светлом" + "<br>" + "обасјана," + "<br>" + "у кораку вечном" + "<br>" + "битисала." + "<br>" + "Умни сусрет" + "<br>" + "две душе је празник," + "<br>" + "знак је неба," + "<br>" + "његове доброте." + "<br>" + "Уживајмо у чину" + "<br>" + "лепоте.";
+
+document.getElementById('ime_pesnika2').innerHTML = "ЉУБОМИР ЈЕРЕМИЋ" + "<br><br>" + "Молитва";
+document.getElementById('ime_pesnika2').href = "";
+//document.getElementById('ime_pesnika2').target = "_blank";
+document.getElementById('pesma2').innerHTML = "Кад у ноћи све затаји," + "<br>" + "и све се умири," + "<br>" + "тад у своје срце нежно" + "<br>" + "и благо завири." + "<br><br>" 
++ "У њему је пуно људи" + "<br>" + "које оно воли." + "<br>" + "Прекрсти се тад лагано" + "<br>" + "и за њих се моли." + "<br><br>" +
+"У молитви није битна," + "<br>" + "знај њена дужина," + "<br>" + "јер искрена и од срца" + "<br>" + "молитва се прима." + "<br><br>" +
+"Не учи се она као" + "<br>" + "песмица и бајка." + "<br>" + "Молимо се просто, нежно" + "<br>" + "ко за децу мајка." + "<br><br>" +
+"Нека молитва наша" + "<br>" + "до неба досеже," + "<br>" + "јер љубав нас једино" + "<br>" + "са Господом веже." + "<br><br>" +
+"Сетимо се и свих оних" + "<br>" + "који с нама нису," + "<br>" + "јер они крај Господа" + "<br>" + "сада стоје близу." + "<br><br>" +
+"Нека молитве се наше" + "<br>" + "сваког дана множе." + "<br>" + "сачувај нас и помилуј," + "<br>" + "молимо те Боже!",
+
 
 console.log(val);
